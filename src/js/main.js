9890b280a146bbf2294d2685cd14550b64c8562c
Andrew Short <andrewshort@Andrews-Mac-mini.local>
2016-04-17 21:36:08 -0700

    use more handlebars

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,66 @@
-window.SurveyExercise = {
-  template: function(name) {
-    return Handlebars.compile($('#template-' + name).html());
-  }
-};
-
 $(function() {
 
   var questionsEl = $('#questions');
+  var questions = testData;
 
-  var questions = [
-    {
-      text: 'what color is your underwear?'
-    },
-    {
-      text: 'what color is your underwear?'
-    },
-    {
-      text: 'what color is your underwear?'
-    },
-    {
-      text: 'what color is your underwear?'
-    }
-  ];
+  var template = Handlebars.compile($('#template-questions').html());
+  Handlebars.registerPartial('question', $('#template-question').html());
+  Handlebars.registerHelper('inc', function(value, options) {
+    return parseInt(value, 10) + 1;
+  });
 
   render();
 
   function render() {
-    questionsEl.html(SurveyExercise.questions(questions).el);
+    questionsEl.html(template({
+      questions: questions
+    }));
   }
 });
+
+window.testData = [
+  {
+    text: 'what color is your underwear?',
+    answers: [
+      {
+        text: 'green',
+        optionType: 'Must Select'
+      },
+    ],
+    noneOfTheAbove: true,
+    shuffle: true
+  },
+  {
+    text: 'what color is your underwear?',
+    answers: [
+      {
+        text: 'green',
+        optionType: 'Must Select'
+      },
+    ],
+    noneOfTheAbove: true,
+    shuffle: true
+  },
+  {
+    text: 'what color is your underwear?',
+    answers: [
+      {
+        text: 'green',
+        optionType: 'Must Select'
+      },
+    ],
+    noneOfTheAbove: true,
+    shuffle: true
+  },
+  {
+    text: 'what color is your underwear?',
+    answers: [
+      {
+        text: 'green',
+        optionType: 'Must Select'
+      },
+    ],
+    noneOfTheAbove: true,
+    shuffle: true
+  }
+];
