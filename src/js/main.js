71abc6453323eab7ae1f51cfe77e56a81d1da36d
Andrew Short <andrewshort@Andrews-Mac-mini.local>
2016-04-18 00:08:18 -0700

    implement deleting questions

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,9 @@
 $(function() {
 
+  // setup
+
   var questions = testData;
+  var currentEvent;
 
   var template = Handlebars.compile($('#template-questions').html());
   Handlebars.registerPartial('question', $('#template-question').html());
@@ -14,14 +17,58 @@ $(function() {
   var rootNode = virtualDom.create(tree);
   questionsEl.append($(rootNode));
 
+  // event handlers
+
+  registerFormEvent('.question-delete', 'click', function(event, el) {
+    var questionIndex = getQuestionIndex(el);
+    questions.splice(questionIndex, 1);
+  });
+
+  // functions
+
+  function render() {
+    var newTree = vdomParser(renderHandlebars());
+    var patches = virtualDom.diff(tree, newTree);
+    rootNode = virtualDom.patch(rootNode, patches);
+    tree = newTree;
+  }
+
   function renderHandlebars() {
     return template({
       questions: questions
     });
   }
+
+  function registerFormEvent(selector, type, handler) {
+    questionsEl.on(type, function(event) {
+      var sourceEl = getSourceEl(event, selector);
+      if (!sourceEl) {
+        return;
+      }
+      handler(event, sourceEl);
+      if (currentEvent !== event.originalEvent) {
+        setTimeout(render, 0);
+        currentEvent = event.originalEvent;
+      }
+    });
+  }
+
+  function getSourceEl(event, selector) {
+    var path = event.originalEvent.path;
+    for (var i = 0; i < path.length; i += 1) {
+      if ($(path[i]).is(selector)) {
+        return path[i];
+      }
+    }
+    return false;
+  }
+
+  function getQuestionIndex(el) {
+    return parseInt($(el).closest('.question').data('index'), 10);
+  }
 });
 
-window.testData = [
+var testData = [
   {
     text: 'what color is your underwear?',
     answers: [
@@ -34,7 +81,7 @@ window.testData = [
     shuffle: true
   },
   {
-    text: 'what color is your underwear?',
+    text: 'why is a raven like a writing desk?',
     answers: [
       {
         text: 'green',
@@ -45,7 +92,7 @@ window.testData = [
     shuffle: true
   },
   {
-    text: 'what color is your underwear?',
+    text: 'why does the porridge bird lay its eggs in the air?',
     answers: [
       {
         text: 'green',
@@ -56,7 +103,7 @@ window.testData = [
     shuffle: true
   },
   {
-    text: 'what color is your underwear?',
+    text: 'what is this i dont even',
     answers: [
       {
         text: 'green',
